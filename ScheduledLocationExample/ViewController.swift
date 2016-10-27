4d7715e523263172adaa143d6be7ad2a828ea2d4
Aleksandrs Proskurins <paleksandrs@gmail.comm>
2016-10-27 22:37:54 +0100

    Example app added

---
+++ ScheduledLocationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Aleksandrs Proskurins
+//
+//  License
+//  Copyright © 2016 Aleksandrs Proskurins
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
